9e6c450e5a05f8f53691a7c9548a37ac2681c6ba
zYx.Tom <zhuyuanxiang@gmail.com>
2021-02-26 18:06:18 +0800

    Ch16.Notebook.End

---
+++ src/Python/my_math.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-    
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+---------------------------
+@Software   :   PyCharm
+@Project    :   BeginningPython
+@File       :   my_math.py
+@Version    :   v0.1
+@Time       :   2021-02-26 17:44
+@License    :   (C)Copyright 2018-2021, zYx.Tom
+@Reference  :   Python 基础教程(第3版)
+@Desc       :   
+@理解：
+"""
+from tools import beep_end
+
+
+# ----------------------------------------------------------------------
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    beep_end()
